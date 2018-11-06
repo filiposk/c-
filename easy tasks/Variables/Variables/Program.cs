287b89659854a7fdc4a2894e5cffdd9ed9803e7d
filiposk <fpavlovic1995@gmail.com>
2018-11-06 15:59:36 +0100

    filiposk

---
+++ easy tasks/Variables/Variables/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string FirstName;
+            FirstName = Console.ReadLine();
+
+            
+            string LastName;
+            Console.Write("Type is your last name: ");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + FirstName + " " + LastName);
+            Console.ReadLine();
+        }
+    }
+}
